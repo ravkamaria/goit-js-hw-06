a06c957c061f3199ebb118cf5e1698a3506513cd
ravkamaria <hevkomaria1@gmail.com>
2023-07-28 15:30:46 +0300

    task-08 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", submitData);
+
+function submitData(evt) {
+  evt.preventDefault();
+  const {
+    elements: { email, password },
+  } = evt.currentTarget;
+  if (email.value === "" || password.value === "") {
+    window.alert("Please, fill in all the fields");
+  } else {
+    const userData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(userData);
+    form.reset();
+  }
+}
