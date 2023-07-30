80bb558dd89edd5b5f4bba1f1db8cf031c849202
ravkamaria <hevkomaria1@gmail.com>
2023-07-30 23:57:34 +0300

    task-10 done

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const boxContainer = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", createBoxes);
+
+function createBoxes(amount) {
+  const allBoxes = [];
+  if (1 <= input.valueAsNumber && input.valueAsNumber <= 100) {
+    amount = input.valueAsNumber;
+  }
+  let sizeFirstBox = 30;
+  for (let i = 1; i <= amount; i++) {
+    sizeFirstBox += 10;
+    const color = getRandomHexColor();
+    const box = `<div class="box" style=" width: ${sizeFirstBox}px; height: ${sizeFirstBox}px; background-color: ${color};"></div>`;
+    allBoxes.push(box);
+  }
+
+  boxContainer.insertAdjacentHTML("beforeend", allBoxes.join(""));
+  return boxContainer;
+}
+
+btnDestroy.addEventListener("click", remover);
+
+function remover() {
+  boxContainer.innerHTML = "";
+  input.value = "";
+}
